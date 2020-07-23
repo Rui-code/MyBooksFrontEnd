95b2b31da48f2b5c8ee8ace38826d73b870d5573
Ruiran Oliveira <ruiranoliveirasantos@gmail.com>
2020-07-22 23:04:55 -0300

    adding upbar and its styles

---
+++ src/upbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+
+function Upbar () {
+    return (
+        <header className="mainHead">
+            <h1>My Books</h1>
+        </header>
+    )
+}
+
+export default Upbar;
