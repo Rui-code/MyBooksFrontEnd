b6a940816f792ca834d68b820318cdb6e5c0ca5c
Ruiran Oliveira <ruiranoliveirasantos@gmail.com>
2020-07-23 22:48:11 -0300

    footer component whith empty styles file (and a helper)

---
+++ src/downbar/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './styles.css'
+
+function Footer () {
+    return (
+        <footer className="footer">
+            <h1>This is the footer</h1>
+        </footer>
+    )
+}
+
+export default Footer;
+
+// {
+//     "workbench.colorTheme": "Dracula",
+//     "workbench.iconTheme": "material-icon-theme",
+//     "explorer.confirmDragAndDrop": false,
+//     "explorer.confirmDelete": false,
+
+//     "emmet.syntaxProfiles": {
+//         "javascript":"jsx"
+//     },
+//     "emmet.includeLanguages": {
+//         "javascript": "javascriptreact"
+//     },
+//     "javascript.updateImportsOnFileMove.enabled": "always",
+//     "C_Cpp.updateChannel": "Insiders",
+//     "git.enableSmartCommit": true
+
+// }
